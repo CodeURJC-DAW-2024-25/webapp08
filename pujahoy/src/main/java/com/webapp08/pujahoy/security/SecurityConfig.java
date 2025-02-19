7703c41eeb020fee581ea8115bc2b447e7033453
jae9104 <j.andres.2022@alumnos.urjc.es>
2025-02-19 16:05:17 +0100

    Corregido el inicio de sesion deprecated

--- pujahoy/src/main/java/com/webapp08/pujahoy/security/SecurityConfig.java
+++ pujahoy/src/main/java/com/webapp08/pujahoy/security/SecurityConfig.java
@@ -4,11 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.net.URI;
 
 @Configuration
 @EnableWebSecurity
@@ -18,21 +15,16 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/public").permitAll()
+                .requestMatchers("/", "/public","/css/**").permitAll()
                 .anyRequest().authenticated()
             )
-            .oauth2Login()  // Habilita el login con OAuth2
-            .and()
+            .oauth2Login(oauth2 -> oauth2
+                
+                .defaultSuccessUrl("/", true)
+            )            
             .logout(logout -> logout
-                .logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository))
+                .logoutSuccessUrl("/")
             );
         return http.build();
-    }
-
-    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler(ClientRegistrationRepository clientRegistrationRepository) {
-        OidcClientInitiatedLogoutSuccessHandler successHandler =
-                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-        successHandler.setPostLogoutRedirectUri("http://localhost:8080/");
-        return successHandler;
     }
 }
